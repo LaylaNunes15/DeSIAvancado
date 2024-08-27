5a9d1521772faca876387336eac0126f96a9f3bd
LaylaNunes15 <148068553+LaylaNunes15@users.noreply.github.com>
2024-08-27 00:32:03 +0000

    código atualizado no dia 26-08-2024

---
+++ aula05/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Program{
+    public static void Main(string[] args){
+        //Conversão implícita
+        int numInt = 45;
+        double numDouble = numInt;
+        Console.WriteLine("Conversão implícita de int para double:");
+        Console.WriteLine($"Int: {numInt}, Double: {numDouble}");
+
+        //Conversão usando o Convert
+        double valorDouble = 7.6;
+        int valorConvertidoInt = Convert.ToInt32(valorDouble);
+        string textoValor ="123";
+        int textoConvertidoInt = Convert.ToInt32(textoValor);
+        Console.WriteLine($"Double: {valorDouble}, Inteiro: {valorConvertidoInt}");
+        Console.WriteLine($"String: {textoValor}, Inteiro: {textoConvertidoInt}");
+
+    }
+}
